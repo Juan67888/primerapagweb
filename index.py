0b3f28e791ffa8000aaaf7a0d1b2ea775b41953a
Juan67888 <jaacosta2000@gmail.com>
2019-07-28 19:35:01 +0200

    primer commit mi sitio web

--- index.py
+++ index.py
@@ -9,6 +9,15 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+@app.route('/listaCompra')
+def ListaCompra():
+    return render_template('ListaCompra.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
